cb94a413e7ab953d2c6d8634e141fffcd4eb630b
sadrasabouri <sabouri.sadra@gmail.com>
2022-02-05 20:42:40 +0330

    [TEST] : print test.

--- train_CIFAR10.py
+++ train_CIFAR10.py
@@ -129,6 +129,7 @@ def main():
     model = Net().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
+    print(data_iterators['test'])
     train(args, model, device, data_iterators, optimizer)
     test(model, device, data_iterators)
 
